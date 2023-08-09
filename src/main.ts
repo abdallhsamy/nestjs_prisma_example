b9ed75d92c43790508a3ac886fe4eeb58a8920e3
Abdallah Samy <abdallhsamy2011@gmail.com>
2023-08-09 13:20:52 +0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv";
 import * as process from "process";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle("Median")
+    .setDescription("The Median API description")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(Number(process.env.APP_PORT) || 80);
 }
 bootstrap();
